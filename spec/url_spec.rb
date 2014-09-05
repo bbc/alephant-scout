6ccdc614ad8568981f6ed757883817445eaef2d3
Oliver Godby <oliver_godby@me.com>
2014-09-05 17:38:38 +0100

    implementing a re-factor suggested by @StevenJack

--- spec/url_spec.rb
+++ spec/url_spec.rb
@@ -2,26 +2,27 @@ require 'spec_helper'
 
 describe Alephant::Scout::Url do
   describe ".valid?(url)" do
+    let(:response) { double('HTTParty::Response') }
     it "is a valid url" do
-      response = double('HTTParty::Response', :code => 200)
+      allow(response).to receive(:code).and_return(200)
       allow(HTTParty).to receive(:head).and_return(response)
       expect(subject.valid?('http://www.avalidurl.com')).to be true
     end
 
     it "is a valid url with an issue" do
-      response = double('HTTParty::Response', :code => 500)
+      allow(response).to receive(:code).and_return(500)
       allow(HTTParty).to receive(:head).and_return(response)
       expect(subject.valid?('http://www.notavalidurl.com')).to be false
     end
 
     it "is an invalid URL - i.e. host is resolved but resource does not exist" do
-      response = double('HTTParty::Response', :code => 404)
+      allow(response).to receive(:code).and_return(404)
       allow(HTTParty).to receive(:head).and_return(response)
       expect(subject.valid?('http://www.avalidurl.com/but/a/nonexistent/resource')).to be false
     end
 
     it "is an invalid host - i.e. no DNS resolution" do
-      response = double('HTTParty::Response', :code => 500)
+      allow(response).to receive(:code).and_return(500)
       allow(HTTParty).to receive(:head).and_raise("Hostname not found")
       expect(subject.valid?('test://www.notavalidurl.test')).to be false
     end
