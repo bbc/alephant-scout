9e54c37b18411fdff435af61e13cb15afec8eeb3
Oliver Godby <oliver_godby@me.com>
2014-09-05 16:14:30 +0100

    updated core implementation in line with comments from @StevenJack and added ruby-version file having run tests and realised that I had not yet specified that

--- lib/alephant/scout/url.rb
+++ lib/alephant/scout/url.rb
@@ -4,7 +4,11 @@ module Alephant
   module Scout
     class Url
       def valid?(url_to_scout)
-        HTTParty.head(url_to_scout).code == 200 ? true : false
+        begin
+          HTTParty.head(url_to_scout).code == 200
+        rescue
+          false
+        end
       end
     end
   end
